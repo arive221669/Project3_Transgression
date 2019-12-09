b16705f6f80b21544b051e445de34e7b35fa9382
Anthony <anthony.rivera3@mail.citytech.cuny.edu>
2019-12-09 11:11:59 -0500

    Adding project and basic assets

---
+++ Project_3_Transgression/Assets/Scripts/Emails.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Emails : MonoBehaviour
+{
+    string value = "This is a string.";
+    int startIndex = 5;
+    int length = 2;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        string substring = value.Substring(startIndex, length);
+        Debug.Log(substring);
+    }
+}
