0583451ead770bdb5395325e79f73fa07eab7189
Anthony <anthony.rivera3@mail.citytech.cuny.edu>
2019-12-09 11:40:15 -0500

    Removed unnecessary assets

--- Project_3_Transgression/Assets/Scripts/Emails.cs
+++ Project_3_Transgression/Assets/Scripts/Emails.cs
@@ -19,4 +19,7 @@ public class Emails : MonoBehaviour
         string substring = value.Substring(startIndex, length);
         Debug.Log(substring);
     }
+
+    //search within the 0 index within the first 10 chars or so to find if player said "I'm sorry" or "I apologize", etc. to determine response
+    //Search 0 index within the last 10 chars to find the same thing. 
 }
