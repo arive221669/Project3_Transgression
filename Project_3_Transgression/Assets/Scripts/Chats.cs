91a6a35a0a9734dfbbdf365da44385bd9d60d57a
Anthony <anthony.rivera3@mail.citytech.cuny.edu>
2019-12-13 11:39:38 -0500

    Adding system time function for chats

--- Project_3_Transgression/Assets/Scripts/Chats.cs
+++ Project_3_Transgression/Assets/Scripts/Chats.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Chats : MonoBehaviour
 {
+
+
     public InputField userInput;
     public Text userReplies;
     public Text customer;
@@ -16,16 +19,27 @@ public class Chats : MonoBehaviour
     private float timer;
     private float timerForCustomerReplies = 3f;
 
+    private string currentTime;
+    private string timeNow;
+    private bool stopTime;
+
+
     public void Update()
     {
-        
 
-    timer += Time.deltaTime;
+        timer += Time.deltaTime;
 
-        Debug.Log(timer);
-
-        if(timer >= 2f && states == 0)
+        while(stopTime == false)
         {
+            currentTime = DateTime.Now.ToLongTimeString();
+        }
+
+        if (timer >= 5f && states == 0)
+        {
+            stopTime = true;
+            timeNow = currentTime;
+            
+            customer.text = timeNow + " The salsa was packed upside down! It opened and spilled everywhere! Everything is ruined in this bag!";
             customer.GetComponent<Text>().enabled = true;
         }
 
@@ -37,36 +51,36 @@ public class Chats : MonoBehaviour
 
         else if (timer >= timerForCustomerReplies && states == 2)
         {
-            customerReplies.text = "Why can't you help?? Are you being serious right now?! That's completely unaccetable, I demand to speak to a manager!";
+            customerReplies.text = timeNow + " Why can't you help?? Are you being serious right now?! That's completely unacceptable, I demand to speak to a manager!";
             customer.GetComponent<Text>().enabled = false;
             customerReplies.GetComponent<Text>().enabled = true;
 
         }
     }
 
+
     public void RepliesToCustomer()
-{
-    userReplies.text = userInput.text;
-}
+        {
+            userReplies.text = "10:31:50 AM [Agent] " + userInput.text;
+        }
 
-public void PlayText()
-{
-
-    weGotIt = userInput.text.Contains("I'm sorry");
-
-
-    if (weGotIt == true)
+    public void PlayText()
     {
-        timer = 0;
-        states = 1;
+
+        weGotIt = userInput.text.Contains("I'm sorry");
+
+
+        if (weGotIt == true)
+        {
+            timer = 0;
+            states = 1;
+
+        }
+        else if (weGotIt == false)
+        {
+            timer = 0;
+            states = 2;
+        }
 
     }
-    else if (weGotIt == false)
-    {
-        timer = 0;
-        states = 2;
-    }
-
 }
-}
-//Testing gitkraken
